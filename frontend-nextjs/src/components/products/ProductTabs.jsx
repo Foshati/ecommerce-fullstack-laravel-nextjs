24efd5661288ba99989a4426fcdf0b9fba8f726a
Foshati <amirrezafoshati@gmail.com>
2024-06-10 17:41:33 +0330

    create tab in daisy ui and next.js

--- frontend-nextjs/src/components/products/ProductTabs.jsx
+++ frontend-nextjs/src/components/products/ProductTabs.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProductItem from "./ProductItem";
 
 export default function ProductTabs({ tabList, tabPanel }) {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <>
-      <div className="flex flex-col items-center justify-center m-8 ">
-        <div role="tablist" className="w-2/5 tabs tabs-boxed tabs-sm">
+      <div className="flex flex-col items-center justify-center m-8">
+        <div role="tablist" className="tabs tabs-bordered">
           {tabList.map((list, index) => (
-            <a key={index} role="tab" className="tab tab-sm ">
-              {list}
-            </a>
+            <input
+              key={index}
+              type="radio"
+              name="product_tabs"
+              role="tab"
+              className={`tab tab-sm ${activeTab === index ? "tab-active" : ""}`}
+              aria-label={list}
+              checked={activeTab === index}
+              onChange={() => setActiveTab(index)}
+            />
           ))}
         </div>
       </div>
@@ -17,10 +27,13 @@ export default function ProductTabs({ tabList, tabPanel }) {
         {tabPanel.map((panel, index) => (
           <div
             key={index}
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+            role="tabpanel"
+            className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ${
+              activeTab === index ? "block" : "hidden"
+            }`}
           >
             {panel.map((product) => (
-              <div key={product.id} className="p-4 ">
+              <div key={product.id} className="p-4">
                 <ProductItem product={product} />
               </div>
             ))}
