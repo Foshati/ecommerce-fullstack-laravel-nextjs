d7a4e31588a9d322a6a9210b50403394e5d12689
Foshati <amirrezafoshati@gmail.com>
2024-06-13 22:47:57 +0330

    fix carousel Image /next.js

--- frontend-nextjs/src/components/layout/carousel/Carousel.jsx
+++ frontend-nextjs/src/components/layout/carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 "use client";
-
 import React, { useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
 
 export const Carousel = ({ data }) => {
   const [slide, setSlide] = useState(0);
@@ -22,29 +22,36 @@ export const Carousel = ({ data }) => {
     <div className="relative w-full overflow-hidden">
       <button
         onClick={prevSlide}
-        className="absolute p-2 text-white transform -translate-y-1/2 bg-black bg-opacity-50 rounded-full top-1/2 left-4"
+        className="absolute z-10 p-2 text-white transform -translate-y-1/2 bg-black bg-opacity-50 rounded-full top-1/2 left-4"
       >
         <ChevronLeft className="w-6 h-6" />
       </button>
-      {data.map((item, idx) => (
-        <img
-          src={item.src}
-          alt={item.alt}
-          key={idx}
-          className={`w-full h-screen object-cover ${slide === idx ? "block" : "hidden"}`}
-        />
-      ))}
+      <div className="relative h-[100vh]">
+        {data.map((item, idx) => (
+          <Image
+            key={idx}
+            src={item.src}
+            alt={item.alt}
+            layout="fill"
+            objectFit="cover"
+            className={`${slide === idx ? "block" : "hidden"}`}
+            priority={idx === 0} // Add priority for the first image
+          />
+        ))}
+      </div>
       <button
         onClick={nextSlide}
-        className="absolute p-2 text-white transform -translate-y-1/2 bg-black bg-opacity-50 rounded-full top-1/2 right-4"
+        className="absolute z-10 p-2 text-white transform -translate-y-1/2 bg-black bg-opacity-50 rounded-full top-1/2 right-4"
       >
         <ChevronRight className="w-6 h-6" />
       </button>
-      <div className="absolute flex space-x-2 transform -translate-x-1/2 bottom-4 left-1/2">
+      <div className="absolute z-10 flex space-x-2 transform -translate-x-1/2 bottom-4 left-1/2">
         {data.map((_, idx) => (
           <button
             key={idx}
-            className={`w-3 h-3 rounded-full ${slide === idx ? "bg-white" : "bg-gray-400"}`}
+            className={`w-3 h-3 rounded-full ${
+              slide === idx ? "bg-white" : "bg-gray-400"
+            }`}
             onClick={() => setSlide(idx)}
           ></button>
         ))}
