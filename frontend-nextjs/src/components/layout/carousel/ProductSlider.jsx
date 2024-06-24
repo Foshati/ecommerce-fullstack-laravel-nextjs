418a5ae6133bd95358daef4e7b42a961bc69361b
Foshati <amirrezafoshati@gmail.com>
2024-06-24 19:39:50 +0330

    create component ProductSlider fixed 3

--- frontend-nextjs/src/components/layout/carousel/ProductSlider.jsx
+++ frontend-nextjs/src/components/layout/carousel/ProductSlider.jsx
@@ -18,20 +18,33 @@ const ProductSlider = ({ images }) => {
     );
   };
 
+  const goToIndex = (index) => {
+    setCurrentIndex(index);
+  };
+
   return (
     <div className="relative w-full max-w-md">
-      <figure>
-        <Image
-          src={images[currentIndex]}
-          placeholder="blur"
-          blurDataURL={getBlurDataUrl()}
-          width={460}
-          height={310}
-          className="w-full rounded-xl cursor-none"
-          alt={`product-image-${currentIndex}`}
-        />
-      </figure>
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <div className="relative w-full h-80">
+        {images.map((image, index) => (
+          <figure
+            key={index}
+            className={`absolute inset-0 transition-opacity duration-500 ${
+              currentIndex === index ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <Image
+              src={image}
+              placeholder="blur"
+              blurDataURL={getBlurDataUrl()}
+              width={460}
+              height={310}
+              className="object-cover w-full h-full rounded-xl"
+              alt={`product-image-${index}`}
+            />
+          </figure>
+        ))}
+      </div>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-2/5">
         <button
           onClick={goToPrevious}
           className="btn btn-circle btn-neutral btn-sm"
@@ -44,6 +57,27 @@ const ProductSlider = ({ images }) => {
         >
           ❯
         </button>
+      </div>
+      <div className="flex justify-center mt-4">
+        {images.map((image, index) => (
+          <button
+            key={index}
+            onClick={() => goToIndex(index)}
+            className={`mx-1 p-1 border ${
+              currentIndex === index ? "border-red-500" : "border-gray-500"
+            } rounded`}
+          >
+            <Image
+              src={image}
+              placeholder="blur"
+              blurDataURL={getBlurDataUrl()}
+              width={50}
+              height={50}
+              className="object-cover w-12 h-12 rounded"
+              alt={`thumbnail-${index}`}
+            />
+          </button>
+        ))}
       </div>
     </div>
   );
