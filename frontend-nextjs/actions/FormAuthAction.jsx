5729c9747687f5cf5f0ae056124cc4c61b0c08f4
Foshati <amirrezafoshati@gmail.com>
2024-07-02 18:29:26 +0330

    postFetch  /auth/login and create login_token in cookies

--- frontend-nextjs/actions/FormAuthAction.jsx
+++ frontend-nextjs/actions/FormAuthAction.jsx
@@ -1,5 +1,9 @@
 'use server';
 
+import {postFetch} from '@/utils/fetch';
+import {handleError} from '@/utils/helpers';
+import {cookies} from 'next/headers';
+
 async function FormAuthAction(stateLogin, formData) {
   const cellphone = formData.get('cellphone');
 
@@ -10,11 +14,30 @@ async function FormAuthAction(stateLogin, formData) {
     };
   }
   const pattern = /^(\+98|0)?9\d{9}$/;
-
   if (!pattern.test(cellphone)) {
     return {
       status: 'error',
       message: 'Invalid phone number',
+    };
+  }
+
+  const data = await postFetch('/auth/login', {cellphone});
+  if (data.status === 'success') {
+    cookies().set({
+      name: 'token',
+      value: data.data.login_token,
+      httpOnly: true,
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    });
+    return {
+      status: data.status,
+      message: 'Login successful',
+    };
+  } else {
+    return {
+      status: data.status,
+      message: handleError(data.message),
     };
   }
 }
