8fdb9fbc8d5a3f7f96e17ee96ea2e5be81bf4450
Foshati <amirrezafoshati@gmail.com>
2024-07-27 12:30:53 +0330

    CreateFormAction for create address

--- frontend-nextjs/src/app/(userDashboard)/profile/addresses/page.jsx
+++ frontend-nextjs/src/app/(userDashboard)/profile/addresses/page.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default async function AddressesPage() {
   const token = cookies().get('token');
-  const {provinces, cities, addresses} = await getFetch('/profile/addresses', {Authorization: `Bearer ${token.value}`});
+  const {provinces, cities} = await getFetch('/profile/addresses', {Authorization: `Bearer ${token.value}`});
   return (
     <div>
       <AddressForm provinces={provinces} cities={cities} />
--- frontend-nextjs/src/components/profile/address/AddressForm.jsx
+++ frontend-nextjs/src/components/profile/address/AddressForm.jsx
@@ -1,88 +1,103 @@
 'use client';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import SubmitButton from '../../button/submitButton/SubmitButton';
+import {toast} from 'sonner';
+import {useFormState} from 'react-dom';
+import {CreateFormAction} from '@/src/actions/ProfileAction';
 
 export default function AddressForm({cities, provinces}) {
   const [show, setShow] = useState(false);
   const [citiesFilter, setCitiesFilter] = useState([]);
+  const [stateCreateForm, formActionCreateForm] = useFormState(CreateFormAction, {});
 
   function changeProvince(e) {
     setCitiesFilter(cities.filter((city) => city.province_id == e.target.value));
     /* console.log(e.target.value); */
   }
 
+  useEffect(() => {
+    if (stateCreateForm?.message && stateCreateForm?.status) {
+      toast(stateCreateForm.message, {type: stateCreateForm.status});
+    }
+  }, [stateCreateForm]);
   return (
     <div>
       <button onClick={() => setShow(!show)} className='btn bg-black'>
         create address
       </button>
-      {show && (
-        <div className='grid lg:grid-cols-2 gap-4 grid-cols-1  sm:grid-cols-2 mx-auto'>
-          <div>
-            <label className='w-full max-w-xs mt-4 mb-4 form-control'>
-              <div className='label'>
-                <span className='label-text'>Title</span>
-              </div>
-              <input name='title' type='text' className='w-full max-w-xs bg-black input input-bordered' />
-            </label>
-            <label className='w-full max-w-xs mt-4 mb-4 form-control'>
-              <div className='label'>
-                <span className='label-text'>Phone number</span>
-              </div>
-              <input name='cellphone' type='text' className='w-full max-w-xs bg-black input input-bordered' />
-            </label>
-            <label className='w-full max-w-xs mt-4 mb-4 form-control'>
-              <div className='label'>
-                <span className='label-text'>Postal code</span>
-              </div>
-              <input name='postal_code' type='text' className='w-full max-w-xs bg-black input input-bordered' />
-            </label>
-          </div>
-          <div>
-            <label className='w-full max-w-xs mt-4 mb-4 form-control'>
-              <div className='label'>
-                <span className='label-text'>Country</span>
-              </div>
-              <div className='w-full max-w-xs'>
-                <select className='select select-bordered w-full max-w-xs bg-black' onChange={changeProvince}>
+      <form action={formActionCreateForm}>
+        {show && (
+          <div className='grid lg:grid-cols-2 gap-4 grid-cols-1  sm:grid-cols-2 mx-auto'>
+            <div>
+              <label className='w-full max-w-xs mt-4 mb-4 form-control'>
+                <div className='label'>
+                  <span className='label-text'>Title</span>
+                </div>
+                <input name='title' type='text' className='w-full max-w-xs bg-black input input-bordered' />
+              </label>
+              <label className='w-full max-w-xs mt-4 mb-4 form-control'>
+                <div className='label'>
+                  <span className='label-text'>Phone number</span>
+                </div>
+                <input name='cellphone' type='text' className='w-full max-w-xs bg-black input input-bordered' />
+              </label>
+              <label className='w-full max-w-xs mt-4 mb-4 form-control'>
+                <div className='label'>
+                  <span className='label-text'>Postal code</span>
+                </div>
+                <input name='postal_code' type='text' className='w-full max-w-xs bg-black input input-bordered' />
+              </label>
+            </div>
+            <div>
+              <label className='w-full max-w-xs mt-4 mb-4 form-control'>
+                <div className='label'>
+                  <span className='label-text'>Country</span>
+                </div>
+                <div className='w-full max-w-xs'>
+                  <select
+                    name='province_id'
+                    className='select select-bordered w-full max-w-xs bg-black'
+                    onChange={changeProvince}>
+                    <option disabled value=''>
+                      Choose a province
+                    </option>
+                    {provinces.map((province) => (
+                      <option key={province.id} value={province.id}>
+                        {province.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </label>
+              <label className='w-full max-w-xs mt-4 mb-4 form-control'>
+                <div className='label'>
+                  <span className='label-text'>City</span>
+                </div>
+                <select name='city_id' className='select select-bordered w-full max-w-xs bg-black'>
                   <option disabled value=''>
-                    Choose a province
+                    Choose a city
                   </option>
-                  {provinces.map((province) => (
-                    <option key={province.id} value={province.id}>
-                      {province.name}
+                  {citiesFilter.map((city) => (
+                    <option key={city.id} value={city.id}>
+                      {city.name}
                     </option>
                   ))}
                 </select>
-              </div>
-            </label>
-
-            <label className='w-full max-w-xs mt-4 mb-4 form-control'>
-              <div className='label'>
-                <span className='label-text'>City</span>
-              </div>
-              <select className='select select-bordered w-full max-w-xs bg-black'>
-                <option disabled value=''>
-                  Choose a city
-                </option>
-                {citiesFilter.map((city) => (
-                  <option key={city.id} value={city.id}>
-                    {city.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-            <label className='form-control'>
-              <div className='label'>
-                <span className='label-text'>Address</span>
-              </div>
-              <textarea
-                name='address'
-                placeholder=''
-                className='textarea textarea-bordered textarea-md w-full max-w-xs bg-black'></textarea>
-            </label>
+              </label>
+              <label className='form-control'>
+                <div className='label'>
+                  <span className='label-text'>Address</span>
+                </div>
+                <textarea
+                  name='address'
+                  placeholder=''
+                  className='textarea textarea-bordered textarea-md w-full max-w-xs bg-black'></textarea>
+              </label>
+            </div>
+            <SubmitButton title='Create address' style='btn btn-wide btn-neutral mb-8' isFormValid={true} />
           </div>
-        </div>
-      )}
+        )}
+      </form>
     </div>
   );
 }
